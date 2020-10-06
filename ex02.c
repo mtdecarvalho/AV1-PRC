b22c423d3001c4ea3a9cfe17cb263645e14c38ee
mtdecarvalho <mtdecarvalho@github.com>
2020-10-06 09:07:17 -0300

    Operador ternário implementado; scannf implementado; nenhum bug discoberto. Revisao final!

--- ex02.c
+++ ex02.c
@@ -10,26 +10,23 @@ int main ()
 {
     char frase1[100], frase2[100], temp, original[100], cont;
     int i, tam, k = 0;
-    do //enquanto fgets detectar uma frase valida
+    do
     {
         printf("Insira a%s frase/palavra a ser analisada: \n", cont == 's' || cont == 'S' ? " proxima" : "");
-        fgets(frase1, sizeof(frase1), stdin); frase1[strlen(frase1)-1] = '\0'; tam = strlen(frase1); strcpy(original, frase1); //retirando o \0 e copiando a frase para outra variavel.
-        for (i = 0; i < strlen(frase1) ; i++) //loop para limpar a frase (retirar pontuacoes e transformar maiusculas em minusculas equivalentes)
+        scanf(" %[^\n]s", frase1); while((getchar()) != '\n'); tam = strlen(frase1); strcpy(original, frase1); 
+        for (i = 0; i < strlen(frase1) ; i++)
         {
             if (frase1[i] >= 65 && frase1[i] <= 90)
                 { frase1[i] += 32; i--; }
         }
-        strcpy(frase2, frase1); // copiando a frase para comparacao
+        strcpy(frase2, frase1);
         tam = strlen(frase2); 
         for (i = 0, k = tam-1 ; i < tam/2 ; i++, k--)
         {
-            temp = frase2[i]; frase2[i] = frase2[k]; frase2[k] = temp; //invertendo a segunda frase para comparacao
+            temp = frase2[i]; frase2[i] = frase2[k]; frase2[k] = temp;
         }
-        if (strcmp(frase1, frase2) == 0)
-            printf("\"%s\" eh palindrome.\n", original);
-        else
-            printf("\"%s\" nao eh palindrome.\n", original);
-        printf("Deseja continuar? [s / n]\n"); scanf("%c", &cont); while ((getchar()) != '\n');
+        printf("\"%s\"%s eh palindrome.\n", original, strcmp(frase2,frase1) == 0 ? "" : " nao"); //substitior por if/else?
+        printf("Deseja continuar? [s / n]\n"); scanf(" %c", &cont); while ((getchar()) != '\n');
     } while (cont == 's' || cont == 'S');
     return 0;
 }
