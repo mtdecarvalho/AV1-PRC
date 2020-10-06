eae2a9df99ed20a378255cada26f62232614557b
mtdecarvalho <mtdecarvalho@github.com>
2020-10-06 16:21:20 -0300

    Precisa de revisao de interação com usuario. 90%

--- ex07.c
+++ ex07.c
@@ -5,11 +5,6 @@
 
 #include <stdio.h>
 
-typedef struct {
-    char nome[30];
-    double n1, n2, media;
-} Taluno;
-
 int ehnum ( char a[] )
 {
     int i;
@@ -19,47 +14,50 @@ int ehnum ( char a[] )
     return 1;
 }
 
-//usar fgets() para ler o input, strtok() para separar as entradas e strtof() para converter para float as notas.
+typedef struct {
+    char nome[30];
+    double n1, n2, media;
+} Taluno;
 
 int main ()
 {
     int n, i = 0, k;
-    char N[10];
+    char N[10], j;
     double mediaturma;
     Taluno alunos[100];
-    printf("Insira o numero de alunos a serem registrados: "); scanf("%s", N);
-    while (ehnum(N)!=1)
-    {   printf("O valor inserido nao eh um numero valido. Tente novamente: ");  scanf("%s", N);   }
-    n = atoi(N);
-    printf("Preencha as proximas %d linhas com os dados dos alunos: \n"
-            "Ex: Matheus 10.0 10.0\n", n);
-    for (i = 0 ; i < n ; i++)
+    do
     {
-        while (scanf("%s %lf %lf", alunos[i].nome, &alunos[i].n1, &alunos[i].n2) != 3)
-        {
-            printf("Os dados foram inseridos incorretamente. Favor seguir o modelo:\nEx: Matheus 10.0 10.0\n");
-            scanf("%s %lf %lf", alunos[i].nome, &alunos[i].n1, &alunos[i].n2);
-        }
-        alunos[i].media = (alunos[i].n1 + 2*alunos[i].n2) / 3;
-    }
-    for (i = 0 ; i < n ; i++)
-    {  
-        printf("%s %lf\n", alunos[i].nome, alunos[i].media);
-        mediaturma += alunos[i].media;
-    }
-    mediaturma /= n; printf("%lf\n", mediaturma);
-    //for (i = 0 ; i < n ; i ++)
-    //{
-    //    if (alunos[i].media > mediaturma)
-    //        k = 1;
-    //    else if (i == n-1 && k != 1)
-    //        k = 0;
-    //}
-    //if (k == 1)
+        printf("Insira o numero de alunos a serem registrados: "); scanf("%s", N); while ((getchar()) != '\n');
+        while (ehnum(N)!=1)
+        {   printf("O valor inserido nao eh um numero valido. Tente novamente: ");  scanf("%s", N); while ((getchar()) != '\n');   }
+        n = atoi(N);
+        printf("Preencha as proximas %d linhas com os dados dos alunos: \n"
+                "Ex: Matheus 10.0 10.0\n", n);
         for (i = 0 ; i < n ; i++)
+        {
+            while (scanf("%s", alunos[i].nome) == 0 || scanf("%lf", &alunos[i].n1) == 0 || scanf("%lf", &alunos[i].n2) == 0
+                         || alunos[i].n1 > 10 || alunos[i].n2 > 10)
+            {
+                while ((getchar()) != '\n');
+                printf("Os dados foram inseridos incorretamente. Favor seguir o modelo:\nEx: Matheus 10.0 10.0\n");
+            }
+            alunos[i].media = (alunos[i].n1 + 2*alunos[i].n2) / 3;
+        }
+        for (i = 0 ; i < n ; i++)
+        {  
+            //printf("%s: %.2lf\n", alunos[i].nome, alunos[i].media);
+            mediaturma += alunos[i].media;
+        }
+        mediaturma /= n; printf("\nMedia da turma: %.2lf\n\n", mediaturma);
+        for (i = 0, k = 0 ; i < n ; i++)
             if (alunos[i].media > mediaturma)
-                printf("%s\n", alunos[i].nome);
-    //else if (k != 1)
-    //    printf("Nenhum aluno possui media maior que a turma.\n");        
+            {   
+                printf("Os seguintes alunos possuem media de notas maior do que a da turma:\n");
+                printf("Nome: %s\tMedia: %.2lf\n", alunos[i].nome, alunos[i].media); k++;
+            }
+            else if (i == n-1 && k == 0)
+                printf("\nNao ha nenhum aluno com media maior do que a media da turma.\n");
+        printf("\nDeseja reiniciar o programa? [s / n]\n"); scanf(" %c", &j); while ((getchar()) != '\n');
+    } while (j == 's' || j == 'S');
     return 0;
 }
