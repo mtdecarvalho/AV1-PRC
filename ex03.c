4543842e833ae631ea37e48de01239e5731022c5
mtdecarvalho <mtdecarvalho@github.com>
2020-10-11 10:41:28 -0300

    100%

--- ex03.c
+++ ex03.c
@@ -9,45 +9,61 @@
 #include <string.h>
 #include <ctype.h>
 
+void limpa ()
+{
+    while ((getchar()) != '\n');
+}
+
 int main ()
 {
     int i, tam, k;
-    char frase[100];
+    char frase[100], j;
     char * palavra;
-    puts("Insira a frase a ser modificada:");
+    
     do
     {
-        k = 0; fgets(frase, 100 , stdin); frase[strlen(frase)-1] = '\0'; tam = strlen(frase);
-        for (i = 0; i < tam ; i++)
-            if (frase[i] >= 48 && frase[i] <= 57)
-                k++;
-        if (k == tam)
-            puts("Por favor insira uma frase valida.");
-    } while (k == strlen(frase));
-    palavra = strtok(frase, " -");
-    printf("Resultado: ");
-    while (palavra != NULL)
-    {
-        for(i = 0; i < strlen(palavra) ; i++)
+        printf("Insira a frase a ser modificada:\n\n");
+        
+        do
         {
-            if (i == 0)
+            k = 0; fgets(frase, 100 , stdin); frase[strlen(frase)-1] = '\0'; tam = strlen(frase);
+            for (i = 0; i < tam ; i++)
+                if (!isalpha(frase[i]))
+                    k++;
+            if (k == tam)
             {
-                if (palavra[i] >= 97 && palavra[i] <= 122)
-                    palavra[i]-=32;
-            }
-            else if ( !isalpha(palavra[i-1]) )
-            {
-                if (palavra[i] >= 97 && palavra[i] <= 122)
-                    palavra[i]-=32;
-            }
-            else
-            {
-                if (palavra[i] >= 65 && palavra[i] <= 90)
-                    palavra[i]+=32;
+                puts("Por favor insira uma frase valida.");
             }
         }
-        printf("%s ", palavra); palavra = strtok(NULL, " -");
+        while (k == strlen(frase));
+        
+        palavra = strtok(frase, " -");
+        printf("\nFrase modificada:\n\n");
+        while (palavra != NULL)
+        {
+            for(i = 0; i < strlen(palavra) ; i++)
+            {
+                if (i == 0)
+                {
+                    if (palavra[i] >= 97 && palavra[i] <= 122)
+                        palavra[i]-=32;
+                }
+                else if ( !isalpha(palavra[i-1]) )
+                {
+                    if (palavra[i] >= 97 && palavra[i] <= 122)
+                        palavra[i]-=32;
+                }
+                else
+                {
+                    if (palavra[i] >= 65 && palavra[i] <= 90)
+                        palavra[i]+=32;
+                }
+            }
+            printf("%s ", palavra); palavra = strtok(NULL, " -");
+        }
+        printf("\n\nDeseja continuar? [s / n]\t"); scanf(" %c", &j); limpa();
     }
-    puts("");
+    while (j == 's' || j == 'S');
+    
     return 0;
 }
