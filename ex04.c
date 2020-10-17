76ac2d3077ac8d6ff58c9cd1201f4175638985d2
mtdecarvalho <matheuscrrezende@gmail.com.br>
2020-10-17 11:42:48 -0300

    100%

--- ex04.c
+++ ex04.c
@@ -2,8 +2,22 @@
 //   caso nao seja possivel somar escreva "Matrizes incompativeis para a soma!" 
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 
 #define MAX 100
+
+void limpartela()
+{
+    #ifdef __unix__
+        system("clear");
+    #elif _WIN32
+        system("cls");
+    #elif _WIN64
+        system("cls");
+    #endif
+}
 
 int ehnum ( char a[] )
 {
@@ -27,38 +41,41 @@ int main ()
 
     do 
     {
+            limpartela();
         do
         {
             printf("Insira o numero de linhas e colunas da primeira matriz: ");
             while ( scanf(" %d", &linha[0]) != 1 || scanf(" %d", &coluna[0]) != 1 || linha[0] < 0 || coluna[0] < 0  || linha[0] > 100 || coluna[0] > 100 )
             {
-                printf("Valores invalidos! Tente novamente.\n"
-                        "Insira o numero de linhas e colunas da primeira matriz: "); limpa();
+                printf("\nValores invalidos! Tente novamente.\n"
+                        "\nInsira o numero de linhas e colunas da primeira matriz: "); limpa();
             }
-            printf("Insira o numero de linhas e colunas da segunda matriz: ");
+            printf("\nInsira o numero de linhas e colunas da segunda matriz: ");
             while ( scanf(" %d", &linha[1]) != 1 || scanf(" %d", &coluna[1]) != 1 || linha[1] < 0 || coluna[1] < 0  || linha[1] > 100 || coluna[1] > 100 )
             {
-                printf("Valores invalidos! Tente novamente.\n"
-                        "Insira o numero de linhas e colunas da segunda matriz: "); limpa();
+                printf("\nValores invalidos! Tente novamente.\n"
+                        "\nInsira o numero de linhas e colunas da segunda matriz: "); limpa();
             }
             if ( linha[0] != linha[1] || coluna[0] != coluna[1] )
-                printf("Matrizes incompativeis para a soma!\n");
+                printf("\nMatrizes incompativeis para a soma!\n");
         }
         while ( linha[0] != linha[1] || coluna[0] != coluna[1] );
 
-        printf("\tMATRIZ 1\n");
+        printf("\n\t\t\tMATRIZ 1\n");
         printf("Preencha as proximas %d linhas com os valores das %d colunas:\n", linha[0], coluna[0]);
         for (i = 0 ; i < linha[0] ; i++)
         {
             for (j = 0 ; j < coluna[0] ; j++)
                 scanf(" %lf", &M1[i][j]);
+            limpa();
         }
-        printf("\tMATRIZ 2\n");
+        printf("\n\t\t\tMATRIZ 2\n");
         printf("Preencha as proximas %d linhas com os valores das %d colunas:\n", linha[1], coluna[1]);
         for (i = 0 ; i < linha[1] ; i++)
         {
             for (j = 0 ; j < coluna[1] ; j++)
                 scanf(" %lf", &M2[i][j]);
+            limpa();
         }
 
         //SOMATORIO
@@ -66,14 +83,14 @@ int main ()
             for (j = 0 ; j < coluna[1] ; j++)
                 res[i][j] = M1[i][j] + M2[i][j];
 
-        printf("\tRESULTADO DA SOMA:\n");
+        printf("\n\t\t\tRESULTADO:\n");
         for (i = 0 ; i < linha[1] ; i++)
         {
             for (j = 0 ; j < coluna[1] ; j++)
                 printf("%.2lf\t",res[i][j]);
             printf("\n");
         }
-        printf("Deseja reiniciar o programa? [s / n]\n"); scanf(" %c", &p); limpa();
+        printf("\nDeseja reiniciar o programa? [s / n]\n"); scanf(" %c", &p); limpa();
         memset(res, 0, sizeof(res)); memset(M1, 0, sizeof(M1)); memset(M2, 0, sizeof(M2));
     }
     while (p == 's' || p == 'S');
