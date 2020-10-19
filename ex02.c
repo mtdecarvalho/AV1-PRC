c7d9cbbdc7373fd0b05cb24363769c09a60027a9
mtdecarvalho <matheuscrrezende@gmail.com.br>
2020-10-19 16:50:51 -0300

    100%

--- ex02.c
+++ ex02.c
@@ -24,15 +24,10 @@ int ehvalido (char a[])
 {
     int i, tam, k = 0;
     tam = strlen(a);
-    for (i = 0 ; i < tam; i++)
-    {
-        if ( isalpha(a[i]) != 0 )
+    for (i = 0; i < tam ; i++)
+        if ( !isalpha(a[i]) )
             k++;
-        else
-            if ( isspace(a[i]) != 0 )
-                k++;
-    }
-    if (k == tam-1 || tam <= 1)
+    if (k == tam)
         return 0;
     else
         return 1;
@@ -51,13 +46,19 @@ int main ()
     {
         limpartela();
         printf("Insira a%s frase/palavra a ser analisada: ", cont == 's' || cont == 'S' ? " proxima" : "");
-        fgets(frase1, tammax, stdin); tam = strlen(frase1); frase1[tam-1] = '\0';
+
+        fgets(frase1, tammax, stdin);
+        if (strlen(frase1) == tammax-1) 
+            limpa();
+        frase1[strlen(frase1)-1] = '\0'; tam = strlen(frase1);
+        
         while ( ehvalido(frase1) != 1 )
         {
-            printf("\n\tInsira uma frase/palavra valida.\n"); fgets(frase1, tammax, stdin); tam = strlen(frase1); frase1[tam-1] = '\0';
+            printf("\n\tInsira uma frase/palavra valida.\n"); fgets(frase1, tammax, stdin); 
+            if (strlen(frase1) == tammax-1) 
+                limpa();
+            frase1[tam-1] = '\0'; tam = strlen(frase1);
         }
-        if (tam == tammax-1)
-            limpa();
         
         strcpy(original, frase1); 
         for (i = 0; i < strlen(frase1) ; i++)
