0430cfb601816c6440b6e38ffde9b04812bf978d
mtdecarvalho <matheuscrrezende@gmail.com.br>
2020-10-20 12:57:35 -0300

    100%

--- ex03.c
+++ ex03.c
@@ -10,6 +10,8 @@
 #include <string.h>
 #include <ctype.h>
 
+#define tammax 100
+
 void limpartela()
 {
     #ifdef __unix__
@@ -21,42 +23,58 @@ void limpartela()
     #endif
 }
 
-void limpa ()
+void limpa()
 {
     while ((getchar()) != '\n');
+}
+
+int analise(char a[])
+{
+    int i, k = 0;
+    if (a[strlen(a)-1] == '\n')
+        a[strlen(a)-1] = '\0';
+    for (i=0;a[i] != '\0';i++)
+        if (isspace(a[i]))
+            k++;
+    if ( k == strlen(a) )
+        return 2;
+    for (i=0;i<strlen(a);i++)
+    {
+        if ( !isalpha(a[i]) && !isspace(a[i]) && !ispunct(a[i]) && !isdigit(a[i]) )
+        {    limpartela(); printf("Frase invalida! \"%s\" possui um ou mais caracteres invalidos.\n\n", a); return 0;   }
+    }
+    return 1;
 }
 
 int main ()
 {
     int i, tam, k;
-    char frase[100], j;
+    char frase[tammax], j;
     
     do
     {
-        limpartela(); printf("Insira a frase a ser modificada:\n\n");
+        limpartela();
         
         do
         {
-            k = 0; fgets(frase, 100 , stdin); frase[strlen(frase)-1] = '\0'; tam = strlen(frase);
-            for (i = 0; i < tam ; i++)
-                if (!isalpha(frase[i]))
-                    k++;
-            if (k == tam)
-            {
-                puts("Por favor insira uma frase valida.");
-            }
+            if (analise(frase) == 2) limpartela();
+            printf("Insira a frase a ser modificada:\n\n");
+            fgets(frase, tammax , stdin); 
+            if (strlen(frase) == tammax-1) 
+                limpa();
         }
-        while (k == strlen(frase));
-        
+        while ( analise(frase) != 1 );
+        tam = strlen(frase);
         printf("\nFrase modificada:\n\n");
-        for(i = 0; i < strlen(frase) ; i++)
+        for(i = 0; frase[i] != '\0' ; i++)
         {
-            if (i == 0)
-            {
-                if (frase[i] >= 97 && frase[i] <= 122)
-                    frase[i]-=32;
-            }
-            else if ( !isalpha(frase[i-1]) )
+            //if (i == 0)
+            //{
+            //    if (frase[i] >= 97 && frase[i] <= 122)
+            //        frase[i]-=32;
+            //}
+            //else
+            if ( !isalpha(frase[i-1]) || i == 0 )
             {
                 if (frase[i] >= 97 && frase[i] <= 122)
                     frase[i]-=32;
